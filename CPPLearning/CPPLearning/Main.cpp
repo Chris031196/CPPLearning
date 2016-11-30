18ab9811b18694c1f4a4d43e321ae32631ea36fd
Chris <Chris@CHRIS-PC>
2016-11-30 21:27:44 +0100

    initial

--- CPPLearning/CPPLearning/Main.cpp
+++ CPPLearning/CPPLearning/Main.cpp
@@ -1,9 +1,46 @@
+#include <Windows.h>
 #include <stdio.h>
 
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
-int main(int length, char* args[]) {
-	for (int i = 0; i < length; i++) {
-		printf(args[i]);
+int WINAPI wWinMain(HINSTANCE instance, HINSTANCE, PWSTR pCmdLine, int nCmdShow) {
+	WNDCLASS wc = {};
+	LPCSTR class_name = "Fenster";
+
+	wc.lpfnWndProc = WindowProc;
+	wc.hInstance = instance;
+	wc.lpszClassName = class_name;
+
+	RegisterClass(&wc);
+
+	HWND hwnd = CreateWindow(
+		class_name, 
+		"Test Fenster", 
+		WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		NULL, 
+		NULL, 
+		instance, 
+		NULL);
+
+	if (!hwnd) return 0;
+
+	ShowWindow(hwnd, nCmdShow);
+
+	MSG msg;
+	BOOL rslt = GetMessage(&msg, NULL, 0, 0);
+	while (rslt) {
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+		rslt = GetMessage(&msg, NULL, 0, 0);
 	}
 	return 0;
+
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	return DefWindowProc(hwnd, uMsg, wParam, lParam);
 }
