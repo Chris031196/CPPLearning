69a6d618fd675578b4f29a87ebd5799c86f7bd00
Chris <Chris@CHRIS-PC>
2016-12-08 20:54:54 +0100

    Clock

--- CPPLearning/CPPLearning/D2DWindow.cpp
+++ CPPLearning/CPPLearning/D2DWindow.cpp
@@ -12,7 +12,10 @@ LRESULT D2DWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 	case WM_CREATE:
 		return OnCreate();
 	case WM_PAINT:
-		OnPaint();
+		return OnPaint();
+		return 0;
+	case WM_SIZE:
+		Resize();
 		return 0;
 	case WM_DESTROY:
 		DiscardGraphics();
@@ -25,8 +28,69 @@ LRESULT D2DWindow::HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 void D2DWindow::DiscardGraphics()
 {
-	SafeRelease(&m_Brush);
+	SafeRelease(&m_BrushBG);
 	SafeRelease(&m_rTarget);
+}
+
+void D2DWindow::InitPixelScale()
+{
+	m_Factory->GetDesktopDpi(&scaleX, &scaleY);
+
+	scaleX /= 96.0f;
+	scaleY /= 96.0f;
+}
+
+void D2DWindow::Resize()
+{
+	RECT rect;
+	GetClientRect(m_hwnd, &rect);
+
+	D2D1_SIZE_U size = D2D1::SizeU(rect.right, rect.bottom);
+
+	m_rTarget->Resize(size);
+
+	CalculateEllipse(size);
+
+	InvalidateRect(m_hwnd, &rect, FALSE);
+}
+
+void D2DWindow::CalculateEllipse(D2D1_SIZE_U size)
+{
+	ellipse = D2D1::Ellipse(D2D1::Point2F(size.width / 2.0f, size.height / 2.0f), size.width / 2.0f, size.height / 2.0f);
+}
+
+void D2DWindow::DrawPointer(float length, float width, float angle)
+{
+	m_rTarget->SetTransform(D2D1::Matrix3x2F::Rotation(angle, ellipse.point));
+
+	m_rTarget->DrawLine(ellipse.point, D2D1::Point2F(ellipse.point.x, ellipse.point.y - ellipse.radiusY*length), m_BrushFG, width);
+}
+
+void D2DWindow::DrawClock()
+{
+	SYSTEMTIME time;
+	GetLocalTime(&time);
+
+	float angleS = (360.0f / 60.0f) * time.wSecond;
+	DrawPointer(1.0f, 2.0f, angleS);
+	
+	float angleH = (360.0f / 12.0f) * time.wHour + (time.wMinute * 0.5f);
+	DrawPointer(0.5f, 6.0f, angleH);
+
+	float angleM = (360.0f / 60.0f) * time.wMinute;
+	DrawPointer(1.0f, 4.0f, angleM);
+
+	m_rTarget->SetTransform(D2D1::Matrix3x2F::Identity());
+}
+
+float D2DWindow::PixelDIPX(float pixel)
+{
+	return pixel/scaleX;
+}
+
+float D2DWindow::PixelDIPY(float pixel)
+{
+	return pixel/scaleY;
 }
 
 BOOL D2DWindow::OnCreate()
@@ -49,18 +113,41 @@ BOOL D2DWindow::OnCreate()
 	}
 
 	D2D1_COLOR_F color = D2D1::ColorF(1.0f, 0.0f, 0.0f);
-
-	hr = m_rTarget->CreateSolidColorBrush(color, &m_Brush);
-
+	hr = m_rTarget->CreateSolidColorBrush(color, &m_BrushBG);
 	if (FAILED(hr)) {
 		return -1;
 	}
+
+	color = D2D1::ColorF(.0f, .0f, .0f);
+	hr = m_rTarget->CreateSolidColorBrush(color, &m_BrushFG);
+	if (FAILED(hr)) {
+		return -1;
+	}
+
+	CalculateEllipse(size);
 
 	return 0;
 }
 
 BOOL D2DWindow::OnPaint()
 {
+	if (m_rTarget != NULL) {
+		PAINTSTRUCT ps;
+		BeginPaint(m_hwnd, &ps);
+		m_rTarget->BeginDraw();
 
-	return true;
+		m_rTarget->Clear(D2D1::ColorF(0.0f, 1.0f, 0.0f));
+		m_rTarget->FillEllipse(ellipse, m_BrushBG);
+		m_rTarget->DrawEllipse(ellipse, m_BrushFG);
+
+		DrawClock();
+
+		HRESULT hr = m_rTarget->EndDraw();
+		EndPaint(m_hwnd, &ps);
+		if (SUCCEEDED(hr)) {
+			return true;
+		}
+	}
+	DiscardGraphics();
+	return -1;
 }
--- CPPLearning/CPPLearning/D2DWindow.h
+++ CPPLearning/CPPLearning/D2DWindow.h
@@ -6,7 +6,7 @@
 class D2DWindow : public BaseWindow<D2DWindow>
 {
 public:
-	D2DWindow() : BaseWindow<D2DWindow>(), m_Factory(NULL), m_rTarget(NULL), m_Brush(NULL) {};
+	D2DWindow() : BaseWindow<D2DWindow>(), m_Factory(NULL), m_rTarget(NULL), m_BrushBG(NULL) {};
 	~D2DWindow();
 
 	LRESULT HandleMessage(UINT uMsg, WPARAM wParam, LPARAM lParam);
@@ -14,12 +14,24 @@ public:
 
 private:
 	void DiscardGraphics();
+	void InitPixelScale();
+	void Resize();
+	void CalculateEllipse(D2D1_SIZE_U size);
+	void DrawPointer(float length, float width, float angle);
+	void DrawClock();
+
+	float PixelDIPX(float pixel);
+	float PixelDIPY(float pixel);
 
 	BOOL OnCreate();
 	BOOL OnPaint();
 
 	ID2D1Factory* m_Factory;
 	ID2D1HwndRenderTarget* m_rTarget;
-	ID2D1SolidColorBrush* m_Brush;
+	ID2D1SolidColorBrush* m_BrushBG;
+	ID2D1SolidColorBrush* m_BrushFG;
+	float scaleX = 1.0f;
+	float scaleY = 1.0f;
+	D2D1_ELLIPSE ellipse;
 };
 
